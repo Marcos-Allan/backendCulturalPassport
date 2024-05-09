19a3ca5e60e06b18bf7b7fdc776775e3e4c71af6
Marcos Allan <allanmenezes880@gmail.com>
2024-05-08 21:29:16 -0300

    fiz a primeira parte do login no backend

---
+++ src/app.js
@@ -0,0 +1,82 @@
+//IMPORTA AS BIBLIOTECAS BAIXADAS E NECESSÁRIAS PARA RODAR A APLICAÇÃO
+const express = require('express')
+const mongoose = require('mongoose')
+
+//INICIA AS VARIÁVEIS DE AMBIENTE PARA SEGURANÇA DA APLICAÇÃO
+require('dotenv').config()
+
+//INICIA A APLICAÇÃO USANDO A BIBLIOTECA EXPRESS
+const app = express()
+
+//DA PERMISSÃO PARA LER JSON EM REQUISIÇÕES
+app.use(express.json())
+
+//MODELO DO OBJETO DO BANCO DE DADOS
+const Person = mongoose.model('Person', {
+    name: String,
+    email: String,
+    img: String,
+});
+
+//MÉTODO DE SIGN-IN
+app.post('/signin', async (req, res) => {
+
+    //PEGA OS DADOS PELA REQUISIÇÃO
+    const name = req.body.name
+    const email = req.body.email
+    const img = req.body.img
+
+    //CRIA UM NOVO USUÁRIO
+    const person = new Person({
+        name: name,
+        email: email,
+        img: img
+    });
+
+    //SALVA O USUÁRIO NO BANCO DE DADOS
+    await person.save()
+
+    //RETORNA O USUÁRIO PARA FEEDBACK
+    res.send(person)
+})
+
+app.post('/signup', async (req, res) => {
+    //PEGA OS DADOS PELA REQUISIÇÃO
+    const emailPesq = req.body.email
+
+    //PROCURA POR UM USUARIO COM O CAMPO ESPECIFICADO
+    const person = await Person.findOne({ email: emailPesq })
+
+    //VERIFICA SE A CONTA ESTÁ CADASTRADA
+    if(person){
+        //RETORNA DADOS DA CONTA COMO FEEDBACK
+        res.send(person)
+    }else{
+        //RETORNA FEEDBACK NEGATIVO PARA O USUÁRIO
+        res.send('Usuario não encontrado, esse atribulado não está cadastrado')
+    }
+})
+
+//REQUISIÇÃO DE TESTE
+app.get('/', (req, res) => {
+    res.send('/')
+})
+
+//LISTA TODOS OS USUÁRIOS CADASTRADOS NO BANCO DE DADOS
+app.get('/users', async (req, res) => {
+    //PEGA OS DADOS DE TODOS OS USUARIOS E LISTA UM POR UM
+    const person = await Person.find()
+    
+    //RETORNA OS USUÁRIOS PARA O USUÁRIO
+    res.send(person)
+})
+
+//RODA O SERVIDOR NA PORTA ESPECIFICADA
+app.listen(process.env.PORT, () => {
+
+    //FAZ A CONEXÃO COM O MONGO DB
+    mongoose.connect(process.env.MONGODB_CONECTION);
+
+    //RETORNA FEEDBACK PARA O USÁRIO
+    console.log('servidor rodando')
+})
