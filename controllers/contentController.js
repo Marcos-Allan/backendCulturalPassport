aa948f11d2b4b12f59f75658bac8f62ff3c1affa
Marcos Allan <allanmenezes880@gmail.com>
2024-07-12 07:36:16 -0300

    criei rotas dos conteudos

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,64 +1,74 @@
 //IMPORTAÇÃO DO MODELO DO BANCO DE DADOS
 const Content = require("../models/Content")
 
-//ROTA PARA SALVAR ARQUIVO NO  SERVIDOR
+const fs = require('fs')
+const path = require('path')
+
+//ROTA PARA CRIAR UM CONTEUDO
 exports.create = async (req, res) => {
     try {
-        //PEGA OS DADOS PELA REQUISIÇÃO
-        const { name } = req.body
-        
-        //PEGA O ARQUIVO PELA REQUISIÇÃO
-        const file = req.file
+        //PEGA O NOME DO ARQUIVO E O ARQUIVO POR MEIO DO ARQUIVO ENVIADO
+        const { filename, originalname } = req.file;
 
+        //LÊ O ARQUIVO E SALVA ELE NO SERVIDOR
+        const content = fs.readFileSync(path.join(__dirname, '../uploads', filename), 'utf8');
+    
         //CRIA UMA NOVO CONTEUDO
-        const content = new Content({
-            name,
-            archive: file.path,
-        })
-
-        //SALVA A IMAGEM NO BANCO DE DADOS
-        await content.save()
-        
-        //RETORNA OS DADOS DA IMAGEM SALVA E UMA MENSAGEM DE SUCESSO
-        res.json({ content, message: "Conteudo salvo com sucesso" })
+        const newContent = new Content({
+            name: originalname,
+            archive: content
+        });
+    
+        //SALVA O CONTEUDO NO BANCO DE DADOS
+        await newContent.save();
+    
+        //REMOVE O CONTEUDO QUE FOI SALVO DO SERVIDOR
+        fs.unlinkSync(path.join(__dirname, '../uploads', filename));
+    
+        //RETORNA MENSAGEM DE SUCESSO AO CRIAR O CONTEUDO
+        res.status(201).send('Arquivo salvo com sucesso');
     } catch (error) {
-        //RETORNA UMA MENSAGEM DE ERRO
-        res.status(500).json({ message: "Erro ao salvar conteudo." })
+        //RETORNA MENSAGEM DE ERRO AO TENTAR CRIAR O CONTEUDO
+        console.error('Erro ao salvar o arquivo:', error);
+        
+        //RETORNA MENSAGEM DE ERRO AO TENTAR CRIAR O CONTEUDO
+        res.status(500).send('Erro ao salvar o arquivo');
     }
 }
 
+//ROTA PARA LISTAR UM CONTEUDO ESPECIFICO
+exports.getOne = async (req, res) => {
+    try {
+        //PROCURA O CONTEUDO PELO NOME PASSSADO POR PARÂMETRO
+        const contents = await Content.findOne({ name: req.params.filename });
+        
+        //VERIFICA SE O CONTEUDO EXISTE OU NÃO
+        if (contents) {
+            //RETORNA O CONTEUDO ESPECIFICADO
+            res.json({ content: contents.archive });
+        }
+        
+        //RETORNA MENSAGEM DE ERRO 404 USUÁRIO NÃO ENCONTRADO
+        return res.status(404).send('Arquivo não encontrado');
+    } catch (error) {
+        //RETORNA UMA MENSAGEM DE ERRO
+        console.error('Erro ao buscar o arquivo:', error);
+        
+        //RETORNA UMA MENSAGEM DE ERRO
+        res.status(500).send('Erro ao buscar o arquivo');
+    }
+}
+
+//ROTA PARA LISTAR TODOS OS CONTEUDOS
 exports.listAll = async (req, res) => {
     try {
+        //PEGA TODOS OS CONTEUDOS DO BD
         const content = await Content.find()
+
+        //RETORNA TODOS OS CONTEUDOS DO BD
         res.send(content)
     } catch (error) {
         //RETORNA UMA MENSAGEM DE ERRO
         res.status(500).json({ message: "Erro ao listar conteudos." })
-    }
-}
-
-exports.getOne = async (req, res) => {
-    try {
-        //PEGA OS DADOS PELA REQUISIÇÃO
-        const name = req.params.name
-
-        //PROCURA POR UM USUARIO COM O CAMPO ESPECIFICADO
-        const content = await Content.findOne({ name: name })
-
-        if(content){
-            //RETORNA O CONTEUDO ESPECIFICADO
-            res.send(content)
-            
-            return
-        }else{
-            //RETORNA ERRO DE CONTEUDO NÃO ENCONTRADO
-            res.send('Conteudo não encontrado')
-            
-            return
-        }
-        
-    } catch (error) {
-        //RETORNA UMA MENSAGEM DE ERRO
-        res.status(500).json({ message: "Erro ao procurar imagem." })
     }
 }
--- routes/content.js
+++ routes/content.js
@@ -7,9 +7,9 @@ const upload = require("../config/multer")
 const ContentController = require('../controllers/contentController')
 
 //DECLARA AS ROTAS E AS FUNÇÕES RESPECTIVAMENTE A SEREM UTILIZADAS PELA ROTA
-router.post('/', upload.single("file"), ContentController.create)
-router.get('/', ContentController.listAll)
-router.get('/:name', ContentController.getOne)
+router.post('/upload', upload.single("file"), ContentController.create)
+router.get('/:filename', ContentController.getOne)
+router.get('/cont/all', ContentController.listAll)
 
 //EXPORTA TODAS AS ROTAS
 module.exports = router
