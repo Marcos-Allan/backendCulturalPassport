cd0962c6f757a75df4a378831388083272b2af4d
Marcos Allan <allanmenezes880@gmail.com>
2024-10-26 11:35:13 -0300

    adicionei rota de correção de erro do arquivo favicon

--- index.js
+++ index.js
@@ -101,6 +101,9 @@ const postData = async (id, message, name) => {
 //ROTA PARA EVITAR PEGAR A IMAGEM INEXISTENTE
 app.get('/favicon.png', (req, res) => res.status(204))
 
+//ROTA PARA EVITAR PEGAR A IMAGEM INEXISTENTE
+app.get('/favicon.ico', (req, res) => res.status(204))
+
 //LÓGICA WebSocket
 wss.on('connection', (ws) => {
     console.log('conectado');
